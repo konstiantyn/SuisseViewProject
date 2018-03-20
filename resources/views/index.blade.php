284cf24c04b290d16457e6e8c51bd948b261cb4c
Bahubali <slark0704@gmail.com>
2018-03-20 12:49:37 +1000

    ggg
    ggg

---
+++ resources/views/index.blade.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Suisse View</title>
+<meta name="keywords" content="Suisse View" />
+<!-- Bootstrap -->
+<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+<!-- <link href="css/jquery.simplyscroll.css" rel="stylesheet"> -->
+<link href="css/animate.css" rel="stylesheet">
+<!-- <link href="css/font/flaticon.css" rel="stylesheet"> -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+<link href="fonts/ic-style.css" rel="stylesheet">
+<!-- <link href="fonts/scrollbar-style.css" rel="stylesheet"> -->
+<link href="css/main-style.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+<header class="header-bg"> 
+	<div class="navigation" data-spy="affix" data-offset-top="50">
+		<div class="container">
+			<div class="row">				
+				<nav class="navbar navbar-inverse">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span> 
+						</button>
+						<a class="navbar-brand" href="url_homepage"><img src="images/logo.png" alt="Logo"></a>
+					</div>
+					<div class="collapse navbar-collapse" id="myNavbar">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="url_homepage">Home</a></li>
+							<li><a href="url_editplaylist">Edit Playlist</a>
+								<ul class="nav-submenu">
+									<li><a href="url_createplaylist">Create Playlist</a></li>
+									<li><a href="url_editplaylist">Edit Playlist</a></li>
+								</ul><!--nav-submenu-->
+							</li>
+							<li><a href="url_projectlist">Project List</a></li>
+							<li><a href="url_logooverlay">Logo Overlay</a></li>
+							<li><a href="url_playlistmsg">Messages</a></li>
+							<li><a href="#" class="user-button"><img src="images/user-top-ic.png"> Justin B</a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
+
+<section class="contaneSection">
+	<div class="container">
+		<div class="row">
+		<h1 class="titleh1">Video Player Scheduler</h1>
+			<div class="col-sm-12 col-md-12">
+				<div class="date-time">
+					<span><i class="fa fa-calendar-alt"></i> 17 / 12 / 2018 </span>
+					<span><i class="fa fa-clock"></i> 11:30 AM </span>
+				</div><!--date-time-->
+			</div>
+			<div class="col-sm-12 col-md-12 video-scale">
+				<div class="videoScaleWrap  table-section">
+					<div class="timeScaleWrap">
+					<div class="timerStrip">
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+						<span class="timerbox">
+							<span>1:00</span>
+							<span>1:15</span>
+						</span>
+					</div><!--timerStrip-->
+
+					<div class="editorStrip">
+						<div class="greenbox editorBox">Play Name<p>sub text here</p></div>
+						<div class="Bluebox editorBox" style="flex-shrink: 0">Play Name<p>sub text here</p></div>
+						<div class="redbox editorBox">Play Name<p>sub text here</p></div>
+						<div class="orangebox editorBox">Play Name<p>sub text here</p></div>
+						<div class="lightbluebox editorBox">Play Name<p>sub text here</p></div>
+						<div class="greybox editorBox">Play Name<p>sub text here</p></div>
+						<div class="Bluebox editorBox" style="flex-shrink: 0">Play Name<p>sub text here</p></div>
+						<div class="redbox editorBox">Play Name<p>sub text here</p></div>
+						<div class="orangebox editorBox">Play Name<p>sub text here</p></div>
+						<div class="lightbluebox editorBox">Play Name<p>sub text here</p></div>
+						<div class="greybox editorBox">Play Name<p>sub text here</p></div>
+					</div><!--editorStrip-->
+				</div><!--timeScaleWrap-->
+			</div><!--videoScaleWrap-->	
+		</div><!--col-12 | video-scale-->
+			<div class="col-sm-12 select-box">
+				<div class="row">
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<select class="form-control" id="#">
+							<option>Project Name</option>
+							<option>Project Name 1</option>
+							<option>Project Name 2</option>
+							<option>Project Name 3</option>
+							<option>Project Name 4</option>
+							<option>Project Name 5</option>
+							<option>Project Name 6</option>
+						</select>
+					</div><!--col-6-->
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<select class="form-control" id="#1">
+							<option>Playlist Name</option>
+							<option>Playlist Name 1</option>
+							<option>Playlist Name 2</option>
+							<option>Playlist Name 3</option>
+							<option>Playlist Name 4</option>
+							<option>Playlist Name 5</option>
+							<option>Playlist Name 6</option>
+						</select>
+					</div><!--col-6-->
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<a class="activate-playlist-button" href="#">
+							<span>Activate Selected Playlist</span>
+						</a>
+					</div><!--col-6-->
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<a class="stop-playlist-button" href="#">
+							<span>Stop Selected Playlist</span>
+						</a>
+					</div><!--col-6-->
+				</div><!--row-->
+			</div><!--col-12-->
+		</div><!--row-->
+		<div class="video-section">
+			<ul class="nav nav-tabs">
+				<li class="active"><a data-toggle="tab" href="#menu1">Playlist Video Clips</a></li>
+				<li><a data-toggle="tab" href="#menu2">Playlist History</a></li>
+			</ul><!--nav nav-tabs-->
+			<div class="tab-content table-section">
+			<div id="menu1" class="tab-pane fade in active">
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-1.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-2.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-3.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-4.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-5.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-6.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-7.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-8.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			</div><!--menu1-->
+			<div id="menu2" class="tab-pane fade">
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-1.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-2.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-3.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-4.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-5.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-6.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-7.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			  <div class="col-xs-6 col-sm-6 col-md-3 wow fadeInUp">
+			  	<div class="video-box">
+			  		<a href="#">
+			  			<img src="images/video-8.jpg">
+			  		</a>
+			  	</div>
+			  </div><!--col-3-->
+			</div><!--menu2-->
+			</div><!--tab-content-->
+		</div><!--video-section-->
+		<div class="project-input">
+			<input type="text" placeholder="Project URL(https:\\suisse-view.com\videoclips\foldername\filename.)">
+		</div>
+	</div><!--container-fluid-->
+</section><!--sectionhome-->
+<footer class="servicefooter">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-sm-12 col-xs-12 copynote wow fadeInDown">
+				Copyright 2018
+			</div>
+		</div>
+	</div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/filterable.pack.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- <script src="js/jquery.simplyscroll.js"></script> -->
+<script src="js/wow.min.js"></script>
+<script src="js/scrollbar.js"></script>
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<script>
+	new WOW().init();
+	$("#scroller").simplyScroll();
+</script>
+</html>
---
+++ resources/views/playlist-messages.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Suisse View</title>
+	<meta name="keywords" content="Suisse View" />
+	<!-- Bootstrap -->
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>
+	<link href="css/jquery.simplyscroll.css" rel="stylesheet">
+	<link href="css/animate.css" rel="stylesheet">
+	<link href="css/font/flaticon.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
+	<link href="fonts/ic-style.css" rel="stylesheet">
+	<link href="fonts/scrollbar-style.css" rel="stylesheet">
+	<link href="css/main-style.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	</head>
+<body>
+	<header class="header-bg"> 
+		<div class="navigation" data-spy="affix" data-offset-top="50">
+			<div class="container">
+				<div class="row">				
+					<nav class="navbar navbar-inverse">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span> 
+							</button>
+							<a class="navbar-brand" href="url_homepage"><img src="images/logo.png" alt="Logo"></a>
+						</div>
+						<div class="collapse navbar-collapse" id="myNavbar">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="url_homepage">Home</a></li>
+								<li><a href="url_editplaylist">Edit Playlist</a>
+									<ul class="nav-submenu">
+										<li><a href="url_createplaylist">Create Playlist</a></li>
+										<li><a href="url_editplaylist">Edit Playlist</a></li>
+									</ul><!--nav-submenu-->
+								</li>
+								<li><a href="url_projectlist">Project List</a></li>
+								<li><a href="url_logooverlay">Logo Overlay</a></li>
+								<li><a href="url_playlistmsg">Messages</a></li>
+								<li><a href="#" class="user-button"><img src="images/user-top-ic.png"> Justin B</a></li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	<section class="contaneSection">
+		<div class="container">
+			<div class="row">
+			<h1 class="titleh1">Playlist Messages</h1>
+				<div class="col-sm-12 select-box create-playlist">
+					<div class="row edit-playlist-section">
+						<div class="col-sm-12 col-md-12">
+							<select class="form-control" id="#">
+								<option>New Messages</option>
+								<option>New Messages 1</option>
+								<option>New Messages 2</option>
+								<option>New Messages 3</option>
+								<option>New Messages 4</option>
+								<option>New Messages 5</option>
+								<option>New Messages 6</option>
+							</select>
+						</div><!--col-12-->
+					</div><!--row | edit-playlist-section-->
+				</div><!--col-12-->
+			</div><!--row-->
+			<div class="col-sm-12 col-md-12">
+				<div class="row">
+				<div class="table-section playlist-m">
+					<div class="table-responsive">          
+						<table class="table">
+						<thead>
+						  <tr>
+							<th class="video-clips-links">Playlist Name</th>						
+						  </tr>
+						</thead>
+						<tbody>
+						  <tr class="active-tr">
+							<td>Rigi-Kulm sunset 1</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						  <tr>
+							<td>Rigi-Kulm sunset XXXXXXX</td>
+						  </tr>
+						</tbody>
+						</table>
+					</div><!--table-responsive-->
+				</div><!--table-section-->
+				</div><!--row-->
+			</div><!--col-12-->
+			<div class="bottom-btns">
+				<a href="#" class="add-video-btn ic-applay-message"><span>Applay Message</span></a>
+				<a href="#" class="del-video-btn ic-delete-message"><span>Delete Message</span></a>
+				<a href="#" class="save-btn ic-save"><span>Save</span></a>
+			</div>
+		</div><!--container-fluid-->
+	</section><!--sectionhome-->
+	<footer class="servicefooter">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-sm-12 col-xs-12 copynote wow fadeInDown">
+					Copyright 2018
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="js/filterable.pack.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.simplyscroll.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/scrollbar.js"></script>
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<script>
+		new WOW().init();
+		$("#scroller").simplyScroll();
+	</script>
+</body>
+</html>
